e98e3b37417f964ee4ca9e3e74c056f58dd9014f
Pierre Buclez <buclez.p@etoncollege.org.uk>
2023-02-07 10:09:29 +0000

    Initial Commit

---
+++ Comment Card Writer/Comment_Card_WriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Comment_Card_WriterApp.swift
+//  Comment Card Writer
+//
+//  Created by Buclez, Pierre (JD) on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Comment_Card_WriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
